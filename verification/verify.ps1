19228a67de46abd95eaa15e3a72175531abd79ed
Petrik van der Velde <petrikvandervelde@gmail.com>
2013-12-17 15:29:11 +1300

    Fixed up powershell verification script so that it actually passes and fails as it should.
    References issue #13

--- verification/verify.ps1
+++ verification/verify.ps1
@@ -3,4 +3,22 @@ Param(
     [string]$arguments
 )
 
-& $installPath $arguments
+$erroractionpreference = "stop"
+
+"Install path is:      $installPath" 
+"Arguments are:        $arguments"
+""
+
+if (($arguments -ne $null) -and ($arguments -ne ""))
+{
+    $process = (Start-Process -FilePath $installPath -ArgumentList $arguments -PassThru -Wait)
+}
+else
+{
+    $process = (Start-Process -FilePath $installPath -PassThru -Wait)
+}
+
+if ($process.ExitCode -ne 0)
+{
+    throw ("Process exited with an exit code of: " + $process.ExitCode)
+}
